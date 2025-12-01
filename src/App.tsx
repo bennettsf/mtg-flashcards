f7955745492f650ad023cbc6429b50aa35fc832e
Bennett Fife <bennett.fife@gmail.com>
2025-11-30 19:12:48 -0800

    wrap mana value provider

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,37 @@ import { SetCardsProvider } from './context/SetCardsContext';
 import { SelectedSetProvider } from './context/SelectedSetContext';
 import GuessContentArea from './components/GuessContentArea';
 import { CardGuessesProvider } from './context/CardGuessesContext';
+import { InputManaValueProvider } from './context/InputManaValueContext';
 
 function App() {
   return (
     <SelectedSetProvider>
       <CardGuessesProvider>
         <SetCardsProvider>
-          <Flex h="100vh" w="100vw" gap={1}>
-            {/* Sidebar */}
-            <SidebarMtgSets />
+          <InputManaValueProvider>
+            <Flex h="100vh" w="100vw" gap={1}>
+              {/* Sidebar */}
+              <SidebarMtgSets />
 
-            {/* Main content */}
-            <Box
-              flex="1"
-              p={4}
-              m={4}
-              display="flex"
-              flexDirection="column"
-              overflow="auto"
-              border={'1px solid gray'}
-            >
-              {/* Top row */}
-              <Box mb={4} display="flex" justifyContent="center" alignItems="center" h="35%">
-                <FlashCard />
+              {/* Main content */}
+              <Box
+                flex="1"
+                p={4}
+                m={4}
+                display="flex"
+                flexDirection="column"
+                overflow="auto"
+                border={'1px solid gray'}
+              >
+                {/* Top row */}
+                <Box mb={4} display="flex" justifyContent="center" alignItems="center" h="35%">
+                  <FlashCard />
+                </Box>
+                {/* Bottom row */}
+                <GuessContentArea />
               </Box>
-              {/* Bottom row */}
-              <GuessContentArea />
-            </Box>
-          </Flex>
+            </Flex>
+          </InputManaValueProvider>
         </SetCardsProvider>
       </CardGuessesProvider>
     </SelectedSetProvider>
